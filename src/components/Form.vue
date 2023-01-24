d6b6c3a457b8bd39e05c8bb1bfc1cab734d17b7d
JedrzejZygadlo <jedrzejzygadlo@gmail.com>
2023-01-24 22:52:50 +0100

    init repo

---
+++ src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Form",
+});
+</script>
+
+<style scoped lang="scss"></style>
